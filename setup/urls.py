1b30d07585e2f39742c14f4da3ba7fe62ab66164
herminioneto <herminio_neto@outlook.com>
2024-03-29 20:18:26 -0300

    Learned how to fetch data from the db and display it in templates

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import home
+from todos.views import todo_list
 
 # Adicionar urls para cada view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', todo_list)
 ]
