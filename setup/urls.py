e95ac23e186ffdcbae5f82e5ae6407eb759172af
herminioneto <herminio_neto@outlook.com>
2024-03-31 14:02:04 -0300

    Created task edit page

--- setup/urls.py
+++ setup/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import TodoListView, TodoCreateView
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView
 
 # Adicionar urls para cada view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TodoListView.as_view(), name="todo_list"),
-    path('create', TodoCreateView.as_view(), name="todo_create")
+    path('create', TodoCreateView.as_view(), name="todo_create"),
+    path('update/<int:pk>', TodoUpdateView.as_view(), name="todo_update")
 ]
